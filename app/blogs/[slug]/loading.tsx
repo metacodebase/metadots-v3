b22afb8691a2e180f38f04d1e87b9141df3eb46e
salmanArif05 <salmanarif1010@gmail.com>
2025-11-17 14:19:46 +0500

    loader added

--- app/blogs/[slug]/loading.tsx
+++ app/blogs/[slug]/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-  return  "Loading...";
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
+    </div>
+  );
 }
